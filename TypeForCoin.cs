28d3786c298991ecf0de1bff8f1c786a09a82232
Emilly Silva <emiisilva1978@gmail.com>
2023-02-14 20:01:00 -0300

    Commit 14/02/2023, introdução de TypeOfCoin e DateTime

---
+++ TypeForCoin.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MeuApp
+{
+    public static class TypeForCoin
+    {
+        public static void Show()
+        {
+            Console.Clear();
+
+            
+            
+            Console.ReadKey();
+            Menu.Show();
+        }
+    }
+}
