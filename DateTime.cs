cfd5d74e7d8748ce2951a3829221951ee631f201
Emilly Silva <emiisilva1978@gmail.com>
2023-02-21 10:36:47 -0300

    Correção de nomenclatura

--- DateTime.cs
+++ DateTime.cs
@@ -11,17 +11,15 @@ namespace MeuApp
             Console.WriteLine(DateTime.DaysInMonth(2020, 2));//ver quantos dias tem no mês, dado o ano e o mês, não é array, é enumerador
             Console.WriteLine(IsWeekend(DateTime.Now.DayOfWeek));
             Console.WriteLine(DateTime.Now.IsDaylightSavingTime());//horario de verão, traz true or false tb
-            
+
             Console.ReadKey();
+            Menu.Show();
 
         }
         static bool IsWeekend(DayOfWeek today)
         {//função parecida com if else, para saber o dia da semana tb
 
             return today == DayOfWeek.Saturday || today == DayOfWeek.Sunday;
-            
-            Console.ReadKey();
-            Menu.Show();
         }
 
 
